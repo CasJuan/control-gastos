06ac3b60765bb1875be872c23a79b0e3bd0e27cb
JuanC <castrojuanig25@gmail.com>
2024-01-14 14:46:41 -0300

    [MOD] reiniciar app

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -2,13 +2,14 @@ import { useState ,useEffect } from "react";
 import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css"
 
-const ControlPresupuesto = ({presupuesto, gastos}) => {
+const ControlPresupuesto = ({presupuesto, gastos, setPresupuesto, setGastos, setIsValidoPresupuesto}) => {
 
   const [porcentaje, setPorcentaje] = useState(0)
   const [disponible, setDisponible] = useState(0);
   const [gastado, setGastado] = useState(0);
 
   useEffect(() => {
+
     const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0 );
 
     const totalDisponible = presupuesto - totalGastado;
@@ -34,6 +35,15 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
     })
   }
 
+  const handleResetApp = () =>{
+    const resultado = confirm('Desea reseter presupuesto y gastos?');
+    if(resultado){
+      setGastos([])
+      setPresupuesto(0)
+      setIsValidoPresupuesto(false)
+    }
+  }
+
 
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
@@ -41,9 +51,9 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
       <div>
         <CircularProgressbar
           styles={buildStyles({
-            pathColor: '#3B82F6',
+            pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
             trailColor: '#F5F5F5',
-            textColor: '#3B82F6',
+            textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
           })}
           value={porcentaje}
           text={`${porcentaje} Gastado`}
@@ -51,11 +61,18 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
       </div>
 
         <div className='contenido-presupuesto'>
+          <button 
+            className="reset-app"
+            type="button"
+            onClick={handleResetApp}
+            >
+            Resetar App
+          </button>
             <p>
               <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
             </p>
 
-            <p>
+            <p className={`${disponible < 0 ? 'negativo' : ''}`}>
               <span>Disponible:</span> {formatearCantidad(disponible)}
             </p>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NuevoPresupuesto from "./NuevoPresupuesto";
 import ControlPresupuesto from "./ControlPresupuesto";
 
 
-const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidoPresupuesto, gastos}) => {
+const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidoPresupuesto, gastos, setGastos}) => {
   return (
     <header>
         <h1>Planificador de Datos</h1>
@@ -11,7 +11,10 @@ const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidoPre
             isValidPresupuesto ? 
             <ControlPresupuesto
                 presupuesto = {presupuesto}
+                setGastos = {setGastos}
                 gastos = {gastos}
+                setPresupuesto = {setPresupuesto}
+                setIsValidoPresupuesto= {setIsValidoPresupuesto}
             />
             :(
                 <NuevoPresupuesto
